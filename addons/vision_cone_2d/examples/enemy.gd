03b60eb9357c2c95433547eaf8a8617f3be0e98e
AlexisAllieri <alexis.allieri@gmail.com>
2024-06-08 22:18:43 +0200

    pathfiding with Navigation2D

--- addons/vision_cone_2d/examples/enemy.gd
+++ addons/vision_cone_2d/examples/enemy.gd
@@ -12,6 +12,8 @@ extends CharacterBody2D
 @export var move_on_path: PathFollow2D
 @export var movement_speed = 0.1
 @onready var pos_start = position.x
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
+
 
 @onready var original_color = vision_renderer.color if vision_renderer else Color.WHITE
 @onready var rot_start = rotation
@@ -24,6 +26,7 @@ extends CharacterBody2D
 @export var speed = 100
 var player_position
 var target_position
+var accel = 5
 
 func _on_vision_cone_area_body_entered(body: Node2D) -> void:
 	print("%s is seeing %s" % [self, body])
@@ -38,6 +41,8 @@ func _on_vision_cone_area_body_exited(body: Node2D) -> void:
 	is_alerted = false
 
 func _physics_process(delta: float) -> void:
+	nav.target_desired_distance = 0.5 
+	var direction = Vector3()
 	if is_rotating:
 		rotation = rot_start + sin(Time.get_ticks_msec()/1000. * rotation_speed) * deg_to_rad(rotation_angle/2.)
 	if move_on_path:
@@ -48,10 +53,21 @@ func _physics_process(delta: float) -> void:
 		is_rotating = false
 		player_position = player.position
 		target_position = (player_position - position).normalized()
+		nav.target_position = player_position
 	if is_searching:
-		if position.distance_to(player_position) > 3:
-			print("cherche")
-			velocity = target_position * speed
+		if position.distance_to(player_position) > 20:
+
+			print(position.distance_to(player_position))
+			
+			direction = nav.get_next_path_position() - global_position
+			direction = direction.normalized()
+			#velocity = direction * speed
+			var new_velocity = velocity.lerp(direction * speed, accel * delta)
+			if nav.avoidance_enabled:
+				nav.set_velocity(new_velocity)
+			else:
+				_on_navigation_agent_2d_velocity_computed(new_velocity)
+			
 			move_and_slide()
 			look_at(player_position)
 			is_rotating = false
@@ -60,3 +76,8 @@ func _physics_process(delta: float) -> void:
 			is_alerted = false
 			is_searching = false
 			is_rotating = true
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity):
+	velocity = safe_velocity
+	pass # Replace with function body.
